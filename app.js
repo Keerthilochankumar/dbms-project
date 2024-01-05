ef0e1b1148b92ec689a624db3a07286d79a51e18
Keerthilochankumar <keerthi.lochans@gmail.com>
2024-01-05 16:34:53 +0530

    initial commit

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const app = express();
 const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require('cors');
-const {Student} = require('./models')
+const {Student,STUDENTATTENDANCE,Faculty,FACULTYATTENDANCE} = require('./models')
 app.use(express.urlencoded());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "/")));
 app.use(cors());
+const today=new Date()
 
 app.get('/', (req, res) => {
   return res.redirect('index.html');
@@ -38,6 +39,22 @@ app.get('/val', async (req, res) => {
     res.status(500).send("Error adding student");
   }
 });
+app.get('/valf', async (req, res) => {
+  try {
+    const faculty = req.query.faculty;
+    const fa_pass = req.query.password;
+    const name = req.query.name;
+    const dept = req.query.dept;
+    console.log(faculty, fa_pass, name, dept);
+    const val = await Faculty.create({ fid: faculty, password: fa_pass, name: name, dept: dept});
+    console.log(val);
+    res.send("Successfully added student");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error adding student");
+  }
+});
+
 
 app.post('/student-login', async (req, res) => {
   try {
@@ -45,29 +62,36 @@ app.post('/student-login', async (req, res) => {
     const st_pass = req.body.password;
     console.log(stu, st_pass);
     const user = await Student.findOne({ where:{sid: stu, password: st_pass }});
-    // if (user) {
-    //   const val = await studentAttendance.create({ sid: stu, login: new Date() });
-    //   console.log(val);
-    //   res.send("Successful student login");
-    // } else {
-    //   res.status(401).send("Invalid credentials");
-    // }
-    console.log(user);
-    return res.status(200).send(user);
+     if (user) {
+       const val = await STUDENTATTENDANCE.create({ sid: stu});
+      res.send("Successful student login");
+     } else {
+       res.status(401).send("Invalid credentials");
+     }
   } catch (error) {
     console.error(error);
     res.status(500).send("Error during student login");
   }
 });
 
-app.get('/faculty-login', (req, res) => {
-  const fac = req.query.sid;
-  const fac_pass = req.query.password;
-
-  // Handle faculty login logic here
-
-  res.send("Successful faculty login");
+app.post('/faculty-login', async(req, res) => {
+  try{const fac = req.body.fid;
+  const fac_pass = req.body.password;
+  const user= await Faculty.findOne({where:{fid:fac,password:fac_pass}});
+  if(user){
+      const val= await FACULTYATTENDANCE.create({fid:fac})
+      console.log(val);
+      res.send("successful faculty login");
+        }
+    else{
+            req.status(401).send("Invalid credentials");
+        }
+   }catch(error){
+        console.log(error);
+        res.status(500).send("Error during facultu login");
+        }
 });
+
 
 app.get('/fac', async (req, res) => {
   try {
